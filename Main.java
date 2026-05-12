d00f1c2162b39e5b54f84f5d60f3e7030d8e8e22
Maíra Laponêz 💜 <mairalcunha@hotmail.com>
2026-05-12 01:26:57 +0000

    Desafio nivel novato

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ public class Main {
         Personagem heroi1 = new Personagem("Zilong", "Guerreiro", 1, 100, 8.5);
         Personagem heroi2 = new Personagem("Gord", "Mago", 1, 100, 6.5);
         Personagem heroi3 = new Personagem("Miya", "Arqueiro", 1, 100, 10.5);
-        heroi1.exirirInformacoes();
-        heroi2.exirirInformacoes();
-        heroi3.exirirInformacoes();
+        heroi1.exibirInformacoes();
+        heroi2.exibirInformacoes();
+        heroi3.exibirInformacoes();
     }
 }
