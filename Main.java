8a52ed7509d064f08f43678a31074f19a5e5426f
Maíra Laponêz 💜 <mairalcunha@hotmail.com>
2026-05-11 22:02:34 -0300

    Update hero characters in Main.java
    Manipulando os dados de cada herói e exibindo os dados.

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
-
-        .
-        .
-        .
-
+        Personagem heroi1 = new Personagem("Zilong", "Guerreiro", 1, 100, 8.5);
+        Personagem heroi2 = new Personagem("Gord", "Mago", 1, 100, 6.5);
+        Personagem heroi3 = new Personagem("Miya", "Arqueiro", 1, 100, 10.5);
+        heroi1.exirirInformacoes();
+        heroi2.exirirInformacoes();
+        heroi3.exirirInformacoes();
     }
 }
