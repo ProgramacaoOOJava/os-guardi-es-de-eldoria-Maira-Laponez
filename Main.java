69b18286a7de193a61333283497eec5cfb7d9751
Maíra Laponêz 💜 <mairalcunha@hotmail.com>
2026-05-13 00:23:07 +0000

    Ajuste no método exibirStatus

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ public class Main {
         Personagem heroi1 = new Personagem("Zilong", "Guerreiro", 1, 100, 8.5);
         Personagem heroi2 = new Personagem("Gord", "Mago", 1, 100, 6.5);
         Personagem heroi3 = new Personagem("Miya", "Arqueiro", 1, 100, 10.5);
-        heroi1.exibirInformacoes();
-        heroi2.exibirInformacoes();
-        heroi3.exibirInformacoes();
+
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
+        heroi3.exibirStatus();
     }
 }
